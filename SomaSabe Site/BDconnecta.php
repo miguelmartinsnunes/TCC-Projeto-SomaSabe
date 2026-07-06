9b75f13f8940fc2c015edf422092e42ee8496a53
miguelmartinsnunes <miguelitomartinsnunes982@gmail.com>
2026-07-06 10:29:47 -0300

    correção php
    correção php

--- SomaSabe Site/BDconnecta.php
+++ SomaSabe Site/BDconnecta.php
@@ -1,7 +1,7 @@
 <?php
 	try {
 		ini_set("display_errors","1");
-		$BDconn=mysqli_connect("192.168.15.13","root","Raul2016","ServidorHeitor");
+		$BDconn=mysqli_connect("localhost","servidorweb","Kaiser251299","SomaSabe");
 		if ($BDconn) {
 			echo("Conexão com o BD realizada. ");
 		} else {
